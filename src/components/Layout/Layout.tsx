bee31f808bb8f3dcc1a0f39558c2a366836f8542
William Alvarez <111912100+wilyyyy@users.noreply.github.com>
2024-07-13 00:49:26 -0700

    stuff

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+import Navigation from "../Navigation/Navigation";
+
+export default function Layout() {
+  <>
+      <Head>
+        <title>My Site</title>
+      </Head>
+      <div>
+        <Navigation />
+        <main>
+          asdasd
+        </main>
+      </div>
+  </>
+
+
+}
